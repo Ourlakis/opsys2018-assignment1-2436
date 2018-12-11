177c0aeb17211de1751001d548329ca829a42136
Ourlakis <35226466+Ourlakis@users.noreply.github.com>
2018-12-12 01:58:50 +0200

    Add files via upload

--- script1a.sh
+++ script1a.sh
@@ -2,36 +2,25 @@
 
 while IFS= read -r line
 do
-	if [[ $line != \#* ]] 
-	then 
-	  	# wget "$line"
-	  	# out1=`curl -I  --stderr /dev/null $line | head -1 | cut -d' ' -f2`
-		
-		# echo $out1
-		# out=`wget -q -c "$line"`
-	  	out=`wget --server-response --spider --quiet "${line}" 2>&1`
-	  	# echo $out
-	  	if [ $? -ne 0 ]; then
-	  		echo $line "FAILED"
-	  	# if [[ `wget -S --spider $line  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then echo "true"; 
-	  	#	else echo "false"; 
-	  	#fi
-	  	else 
-	  		filename=`wget  -q -nv -N $line 2>&1 |cut -d\" -f2`
-	  		# if [[ `wget -N $line  2>&1 | grep "304 Not Modified" ` ]]
-	  		echo $filename 
-	  		if [ ! -e "./$filename" ] 
-	  		then
-		  		wget -N -nv -q $line 2>&1 |cut -d\" -f2 
-	  			echo $line INIT
-	  		else
-	  	#then 
-	  		# File is the same - Nothing is shown
-	  	#	:
-	  	#else
-	  			wget -nv -N $line -q 2>&1 |cut -d\" -f2 
-	  			echo $line
-	  		fi
+	if [[ $line != \#* ]]
+	then
+
+			if curl --output /dev/null --silent --head --fail "$line"; then
+
+				filename=${line////_}
+				filename=${filename//:/_}
+
+				if [ -e "$filename" ]; then
+					if [[ "$(curl $line -z $filename -o $filename -s -L -w %{http_code})" == "200" ]]; then
+  					# code here to process index.html because 200 means it gets updated
+  					echo $line
+					fi
+				else
+					echo $line "INIT"
+					curl "$line" -o "$filename" -s
+				fi
+	  	else
+		  	echo $line "FAILED"
 	  	fi
 	fi
 
